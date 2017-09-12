2d8c297eb236a22c39310f3e14022b39905ae16e
Jaxel A Rojas <jaxelr@gmail.com>
2017-09-11 21:59:24 -0400

    Included project for Insight.Database.

---
+++ OrmBenchmark.Insight.Database/InsightDatabaseExecuter.cs
@@ -0,0 +1,49 @@
+﻿using Insight.Database;
+using OrmBenchmark.Core;
+using OrmBenchmark.Insight.Database;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+
+namespace OrmBenchmark.InsightDatabase
+{
+    public class InsightDatabaseExecuter : IOrmExecuter
+    {
+        private SqlConnection conn;
+
+        public string Name => "Insight Database";
+
+        public void Finish()
+        {
+            conn.Close();
+        }
+
+        public IList<dynamic> GetAllItemsAsDynamic()
+        {
+            return conn.QuerySql<dynamic>("select * from Posts");
+        }
+
+        public IList<IPost> GetAllItemsAsObject()
+        {
+            return conn.QuerySql<Post>("select * from Posts").ToList<IPost>();
+        }
+
+        public dynamic GetItemAsDynamic(int Id)
+        {
+            object param = new { Id = Id };
+            return conn.QuerySql<dynamic>("select * from Posts where Id=@Id", param).FirstOrDefault();
+        }
+
+        public IPost GetItemAsObject(int Id)
+        {
+            object param = new { Id = Id };
+            return conn.QuerySql<Post>("select * from Posts where Id=@Id", param).FirstOrDefault();
+        }
+
+        public void Init(string connectionStrong)
+        {
+            conn = new SqlConnection(connectionStrong);
+            SqlInsightDbProvider.RegisterProvider();
+        }
+    }
+}
